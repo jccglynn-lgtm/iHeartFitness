6eca24844b0cfe31f0bad7a8c38331dd3c269e8f
themanager2 <aarongafurova@gmail.com>
2025-11-19 16:51:08 +0000

    Added javascript to buttons.

--- js/Tracker.js
+++ js/Tracker.js
@@ -10,3 +10,27 @@ tabs.forEach(tab => {
     document.getElementById(panelId).classList.add('active');
 }
 })
+let WaterTotal = 0;
+// click boxes
+const WaterBoxes = document.querySelectorAll('#water .water-box');
+const WaterDisplay = document.getElementById('water-total');
+// input
+const customInput = document.querySelector('#water .custom-input');
+//handle boxes
+WaterBoxes.forEach(box => {
+    box.addEventListener('click', () => {
+        const value = Number(box.dataset.value);
+        WaterTotal += value;
+        WaterDisplay.textContent = WaterTotal;
+
+    })
+});
+//handle custom inputs
+customInput.addEventListener('change', () => {
+    const value = Number(customInput.value);
+    if(value > 0) {
+        WaterTotal += value;
+        WaterDisplay.textContent = WaterTotal;
+        customInput.value = ""; // clearing after adding
+    }
+});
