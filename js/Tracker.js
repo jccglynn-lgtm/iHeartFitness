03d2d0d6ee6345f0f18f6cbe43ea6b4ad4ab2f0e
themanager2 <aarongafurova@gmail.com>
2025-11-26 15:53:31 +0000

    Adding small tracker changes.

--- js/Tracker.js
+++ js/Tracker.js
@@ -1,5 +1,7 @@
 const tabs = document.querySelectorAll('.tracker-tab');
 const panels = document.querySelectorAll('.tracker-panel');
+let weeklyWater = [0, 0, 0, 0, 0, 0, 0];
+let today = (new Date().getDay() + 6) % 7;
 
 tabs.forEach(tab => {
     tab.onclick = () => {
@@ -10,12 +12,14 @@ tabs.forEach(tab => {
     document.getElementById(panelId).classList.add('active');
 }
 })
+// Water Tracker Code
+
 let WaterTotal = 0;
 // click boxes
 const WaterBoxes = document.querySelectorAll('#water .water-box');
 const WaterDisplay = document.getElementById('water-total');
 // input
-const customInput = document.querySelector('#water .custom-input');
+const customInputW = document.querySelector('#water .custom-inputW');
 //handle boxes
 WaterBoxes.forEach(box => {
     box.addEventListener('click', () => {
@@ -26,11 +30,50 @@ WaterBoxes.forEach(box => {
     })
 });
 //handle custom inputs
-customInput.addEventListener('change', () => {
-    const value = Number(customInput.value);
+customInputW.addEventListener('change', () => {
+    const value = Number(customInputW.value);
     if(value > 0) {
         WaterTotal += value;
         WaterDisplay.textContent = WaterTotal;
-        customInput.value = ""; // clearing after adding
+        customInputW.value = ""; // clearing after adding
     }
 });
+
+// Calory Tracker Code
+
+let CaloryTotal = 0;
+// click boxes
+const CalBoxes = document.querySelectorAll('#calories .cal-box');
+const CalDisplay = document.getElementById('cal-total');
+// input
+const customInputC = document.querySelector('#calories .custom-inputC');
+//handle boxes
+CalBoxes.forEach(box => {
+    box.addEventListener('click', () => {
+        const value = Number(box.dataset.value);
+        CaloryTotal += value;
+        CalDisplay.textContent = CaloryTotal;
+
+    })
+});
+//handle custom inputs
+customInputC.addEventListener('change', () => {
+    const value = Number(customInputC.value);
+    if(value > 0) {
+        CaloryTotal += value;
+        CalDisplay.textContent = CaloryTotal;
+        customInputC.value = ""; // clearing after adding
+    }
+});
+function updateWeeklyBars() {
+    const ids = [
+        "bar-mon", "bar-tue", "bar-wed", "bar-thu",
+        "bar-fri", "bar-sat", "bar-sun"
+    ];
+
+    for (let i = 0; i < 7; i++) {
+        let amount = weeklyWater[i];
+        let percent = Math.min((amount / 2000) * 100, 100);  
+        document.getElementById(ids[i]).style.width = percent + "%";
+    }
+}
