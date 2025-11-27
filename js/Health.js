de6827ab30097314dfdf0d5884140a6c40a98206
jccglynn-lgtm <jccglynn@gmail.com>
2025-11-27 22:12:05 +0000

    gif's added for health page

--- js/Health.js
+++ js/Health.js
@@ -3,7 +3,26 @@ const box = document.getElementById("infoBox");
 
 areas.forEach(function(area) {
   area.onclick = function() {
-    box.textContent = area.dataset.text;
+
+    const text = area.dataset.text;
+    const gifSrc = area.dataset.gif;
+    const title = area.title;
+
+    let newContent = '';
+    
+    newContent += '<h2>' + title.charAt(0).toUpperCase() + title.slice(1) + ' Exercises</h2>';
+
+    if (gifSrc) {
+
+      newContent += '<div class="gif-container">';
+      newContent += '<img src="' + gifSrc + '" alt="' + title + ' exercise GIF" class="info-gif">';
+      newContent += '</div>';
+    }
+
+    newContent += '<p>' + text + '</p>';
+
+    box.innerHTML = newContent;
+
     box.style.display = "block";
   };
 });
